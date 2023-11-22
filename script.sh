9a36556121df2e8f856f8b23dba6b06fc28f2ed7
Radhitya Rahman <radhityar6767@gmail.com>
2023-11-22 11:42:27 +0000

    Fix Jenkinsfile

--- script.sh
+++ script.sh
@@ -5,6 +5,8 @@ a=$(sed -n 's|<artifactId>\(.*\)</artifactId>|\1|p' pom.xml)
 b=$(sed -n 's|<version>\(.*\)</version>|\1|p' pom.xml)
 
 # Use sed to replace placeholders in file1.txt
+a=$(echo $a | cut -d ' ' -f1)
+b=$(echo $b | cut -d ' ' -f1)
 sed -i "s|<<ARTIFACTID>>|$a|g; s|<<VERSION>>|$b|g" Dockerfile
 
 cat Dockerfile
